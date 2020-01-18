c089c1bba2b96c779e888d7467c6e28d73f0ab9b
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-18 12:52:52 +0530

    'Assignment'

--- AssignmentOperator.java
+++ AssignmentOperator.java
@@ -2,7 +2,7 @@ class AssignmentOperator{
     public static void main(String[] args){
         int n1,n2;
         n1=5;
-        n2=10;
+        n2=n1;
         System.out.println(n1);
         System.out.println(n2);
     }
