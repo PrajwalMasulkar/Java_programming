4067e5ab010fd9328c7e593c7ffbf8e88b2f3b8a
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-18 13:53:35 +0530

    'Unary'

---
+++ UnaryOperator.java
@@ -0,0 +1,14 @@
+class UnaryOperator
+{
+    public static void main(String[] args)
+    {
+        double number = 5.2;
+        boolean flag = false;
+        System.out.println("+number = "  + +number);
+        System.out.println("-number = "  + -number);
+        System.out.println("++number = "  + ++number);
+        System.out.println("--number = "  + --number);
+        System.out.println("!FLag = "  + !flag);
+    }
+
+}
