ce7939748edaf1baf220aa8711eeb00346f4b8e3
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-18 13:08:12 +0530

    '+-*/%'

---
+++ ArithmeticOperator.java
@@ -0,0 +1,18 @@
+class ArithmeticOperator
+{
+    public static void main(String[] args)
+    {
+    double n1=12.5,n2=3.5,result;
+    result = n1+n2;
+    System.out.println(result);
+    result = n1+n2;
+    System.out.println(result);
+    result = n1*n2;
+    System.out.println(result);
+    result = n1/n2;
+    System.out.println(result);
+    result = n1%n2;
+    System.out.println(result);
+
+     }
+}
