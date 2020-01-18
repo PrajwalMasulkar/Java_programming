8460e71e88fdf8fc6db3c49c42a3742360ddf44c
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-18 12:51:36 +0530

    'Assignment'

---
+++ AssignmentOperator.java
@@ -0,0 +1,10 @@
+class AssignmentOperator{
+    public static void main(String[] args){
+        int n1,n2;
+        n1=5;
+        n2=10;
+        System.out.println(n1);
+        System.out.println(n2);
+    }
+
+}
