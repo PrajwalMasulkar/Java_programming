27b20e68417a9258befd95e15bc4ce916af64d1f
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-18 13:17:17 +0530

    'String'

---
+++ S_tring.java
@@ -0,0 +1,13 @@
+class S_tring
+{
+    public static void main(String[] args)
+    {
+       String start,middle,end,result;
+       start= "Sahadeb is ";
+       middle="Senior ";
+       end = "Developer ";
+       result=start + middle+end;
+
+               System.out.println(result);
+    }
+}
