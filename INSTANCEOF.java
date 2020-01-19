c583937b25222c185138919cb4be30eb0318ae94
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-19 16:40:18 +0530

    'Array'

---
+++ INSTANCEOF.java
@@ -0,0 +1,10 @@
+class INSTANCEOF
+{
+ public static void main(String[] args)
+    {
+        String test="asdf";
+        Boolean result;
+        result = test instanceof String;
+        System.out.print(result);
+    }
+}
