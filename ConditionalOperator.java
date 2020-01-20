e6e4140fe082734585a7b5ad0ee91c4eb24d8fe1
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-20 10:38:05 +0530

    'new'

---
+++ ConditionalOperator.java
@@ -0,0 +1,10 @@
+class ConditionalOperator
+{
+    public static void main(String[] args)
+        {
+            int febuary=29;
+
+            String result;
+            result = (febuary == 28)? "leapyear":"not a leap year";
+            System.out.println(result);
+        }}
