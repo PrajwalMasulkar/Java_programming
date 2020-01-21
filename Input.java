886aa1d0e04c30181bc5c70533b8acc3c3d1a4ac
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-21 18:51:44 +0530

    'new'

---
+++ Input.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Input
+        {   public static void main(String[] args)
+        {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter Float: ");
+            float myfloat = scan.nextFloat();
+            System.out.println(myfloat);
+            System.out.println("Enter Double: ");
+            double mydouble = scan.nextDouble();
+            System.out.println(mydouble);
+            System.out.println("Enter Text");
+            String ab= scan.next();
+            System.out.print(ab);
+
+        }
+        }
