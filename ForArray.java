5614b6f3ff33578d18ca31000a4e1b1fae4d8256
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-21 18:49:01 +0530

    'new'

---
+++ ForArray.java
@@ -0,0 +1,8 @@
+class ForArray{
+    public static void main(String[] args){
+        char[] vowels = {'a','e','i','o','u'};
+        for(char item: vowels)
+            System.out.println(item);
+    }
+
+}
