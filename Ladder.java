0844d655a51629a180f56952a5ab056e6991a6aa
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-22 19:34:15 +0530

    'new'

---
+++ Ladder.java
@@ -0,0 +1,11 @@
+class Ladder{
+    public static void main(String[] args){
+        int n1=0;
+        if(n1>0)
+            System.out.println("This is greter than 0");
+        else if(n1<0)
+            System.out.println("This is less than zero");
+        else
+            System.out.println("This is Zero");
+    }
+}
