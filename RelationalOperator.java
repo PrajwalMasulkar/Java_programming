94432860ed46c11dcf4946dca0fbc276beab105e
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-23 18:46:11 +0530

    'new'

---
+++ RelationalOperator.java
@@ -0,0 +1,13 @@
+class RelationalOperator
+{
+    public static void main(String[] args)
+    {
+        int n1=10,n2=5;
+        if(n1>n2)
+        {
+            System.out.println("n1 is greater than n2");
+        }
+        else
+            System.out.println("n2 is greater than n1");
+    }
+}
