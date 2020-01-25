5013a020a4c7f16ac771c48046ff74fb729a2eda
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-25 18:29:33 +0530

    'new'

---
+++ Test2.java
@@ -0,0 +1,13 @@
+class Test2{
+    static int x =10;
+    int y=20;
+    public static void main(String[] args){
+
+            Test2 t1=new Test2();
+            t1.x =888;
+            t1.y = 999;
+            Test2 t2 = new Test2();
+         System.out.print("x ="+ t2.x+ " y:"+t2.y);
+                    }
+
+    }
