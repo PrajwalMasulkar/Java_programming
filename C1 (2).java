0cf3825e90d79a56bc292545e3a56f8106cab8d3
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-26 23:56:16 +0530

    'new'

---
+++ C1 (2).java
@@ -0,0 +1,28 @@
+class A
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is A Method");
+	}
+}
+
+class B
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is B Method");
+	}
+}
+public class C1
+{
+
+	
+
+}
+class D
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is D Method");
+	}
+}
