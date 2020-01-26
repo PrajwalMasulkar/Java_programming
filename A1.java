3bd57ef0bab352ecdfdb4eff7bcc1c22131ee4af
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-26 23:46:02 +0530

    'nw'

---
+++ A1.java
@@ -0,0 +1,28 @@
+public class A1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is A Method");
+	}
+}
+
+class B
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is B Method");
+	}
+}
+class C
+{
+
+	
+
+}
+class D
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is D Method");
+	}
+}
