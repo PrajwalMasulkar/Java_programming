00989e6405c3912ae8fe1f1c74d38d067cbafd12
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-02-09 13:50:32 +0530

    'non-static_cannot_ACCESS'

---
+++ Studet.java
@@ -0,0 +1,12 @@
+class Studet{
+    int roll;
+    String name;
+    static void update()
+    {
+        System.out.println(roll);
+    }
+    public static void main(String args[])
+    {
+        Studet.update();
+    }
+}
