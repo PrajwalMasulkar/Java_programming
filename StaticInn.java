418f874d5a7bf4e7763b5f1ec02383a331e20101
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-02-09 11:32:55 +0530

    'new'

---
+++ StaticInn.java
@@ -0,0 +1,25 @@
+class StaticInn{
+    static int a =10;
+    static class Inner
+    {
+        int p;
+        static void f1()
+        {
+            System.out.print("Inner class");
+            System.out.println(a);
+        }
+        Inner(){
+            this(20);
+            System.out.println("Java Programming");
+        }
+        Inner(int p){this.p=p;
+        System.out.println("Java Programming - 1");
+        System.out.println(p);
+
+        }
+    }
+    public static void main(String args[])
+    {
+        StaticInn.Inner lo = new StaticInn.Inner();
+    }
+}
