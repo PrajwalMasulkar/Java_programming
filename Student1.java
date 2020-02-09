479af30e286aaa5c6e427a7c19dc1c119e47fed3
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-02-09 13:54:26 +0530

    'new'

---
+++ Student1.java
@@ -0,0 +1,12 @@
+class Student1{
+    static int roll;
+    String name;
+    static void update()
+    {
+        System.out.println(roll);
+    }
+    public static void main(String args[])
+    {
+        Student1.update();
+    }
+}
