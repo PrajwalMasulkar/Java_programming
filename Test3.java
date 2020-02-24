a114e4048578e9df243f1036b449af32a7dd7726
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-02-25 04:36:34 +0530

    'new'

---
+++ Test3.java
@@ -0,0 +1,18 @@
+class Derived{
+    public void getDetails(){
+        System.out.printf("Derived class");
+    }
+}
+
+public class Test3 extends Derived{
+    public void getDetails(){
+  System.out.printf("Test class ");
+  super.getDetails();
+    }
+
+public static void main(String[] args){
+    Derived obj = new Test3();
+    obj.getDetails();
+
+}
+}
