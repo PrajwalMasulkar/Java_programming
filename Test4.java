ce63f2b8a3499fcdeaa7594502898996c8a7d63e
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-02-25 04:41:12 +0530

    'new'

---
+++ Test4.java
@@ -0,0 +1,10 @@
+class Test4{
+    protected int x,y;
+
+}
+class Main{
+    public static void main(String[] args){
+        Test4 t = new Test4();
+        System.out.println(t.x+ " " + t.y);
+    }
+}
