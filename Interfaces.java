a56e473cfb4c7480166c90f1f6ac695d2e66eabc
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-03-03 07:10:39 +0530

    'new'

---
+++ Interfaces.java
@@ -0,0 +1,17 @@
+interface Interfaces
+{
+    void show();
+}
+class Test implements Interfaces
+{
+    public void show()
+    {
+        System.out.println("1");
+
+    }
+    public static void main(String[] args)
+    {
+        Test t = new Test();
+        t.show();
+    }
+}
