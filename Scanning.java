c114206f616f2de4458cd33716266db954b1b7cb
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-03-05 08:05:18 +0530

    'new'

---
+++ Scanning.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Scanning{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        char ch=scan.next().charAt(0);
+        System.out.println("c = "+ch);
+    }
+}
