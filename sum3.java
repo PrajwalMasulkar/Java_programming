b8e50c8d7dc779ecc260c484755791cfbd82bd56
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-04-19 23:43:50 +0530

    'new'

---
+++ sum3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class sum3{
+    public static void main(String[] args)
+    {
+        int n1,n2;
+        Scanner scan = new Scanner(System.in);
+        n1=scan.nextInt();
+        n2=scan.nextInt();
+        int sum=n1+n2;
+        System.out.print("Sum is: "+sum);
+    }
+
+}
