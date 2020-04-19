473af56a8d9b203833efccb0366fa97d78504394
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-04-19 23:44:40 +0530

    'new'

--- sum3.java
+++ sum3.java
@@ -5,6 +5,7 @@ class sum3{
     {
         int n1,n2;
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 1st number ");
         n1=scan.nextInt();
         n2=scan.nextInt();
         int sum=n1+n2;
